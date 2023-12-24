986208d03acd91c4d2d36620af98de4dea43c6ea
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-12-24 15:42:04 +0100

    changement

--- 207374/Documentation/Victoria_site/js1.js
+++ 207374/Documentation/Victoria_site/js1.js
@@ -1,54 +1,53 @@
-var style;
-    // Fonction pour afficher les articles en fonction du choix de style
-    let afficherArticles = () =>{
-        document.getElementById('articlesContainer').innerHTML +=``;
-        var gallery = document.getElementById('styleChoice').value;
-        if (gallery == "Classy"){
-            style = classy;
-        }
-        else if (gallery=="streetwear"){
-            style = StreetWear;
-        }
-        else if (gallery=="Casual"){
-            style = casual;
-        }
-        else if (gallery=="Sporty"){
-            style = sporty;
-        }
-        for (let article of style){
-            document.getElementById('articlesContainer').innerHTML+= `
-            <div class="col-md-9">
-            <div class="product-layouts">
-                <div class="product-thumb">
-                    <div class="image zoom">
-                        <a href="#">
-                            <img src="${article.imageSrc}" alt="${article.altText}" height="501" width="385"/>    
-                            <img src="${article.secondImageSrc}" alt="${article.secondAltText}" class="second_image img-responsive" height="501" width="385"/>
-                        </a>
-                    </div>
-                    <div class="thumb-description">
-                        <div class="caption">
-                            <h4 class="product-title text-capitalize"><a href="${article.productLink}">${article.productTitle}</a></h4>
+let afficherArticles = () => {
+    document.getElementById('articlesContainer').innerHTML = ''; // Efface le contenu actuel avant d'ajouter les articles
+    var gallery = document.getElementById('styleChoice').value;
+
+    if (gallery == "Classy"){
+        style = classy;
+    }
+    else if (gallery == "streetwear"){
+        style = StreetWear;
+    }
+    else if (gallery == "Casual"){
+        style = casual;
+    }
+    else if (gallery == "Sporty"){
+        style = sporty;
+    }
+
+    for (let article of style){
+        let articleHTML = `
+            <div class="col-md-3 mb-4">
+                <div class="product-layouts">
+                    <div class="product-thumb">
+                        <div class="image zoom">
+                            <a href="#">
+                               <img src="${article.imageSrc}" alt="${article.altText}" height="501" width="385" class="image-hover"
+     onmouseenter="ajouterAnimation(this)" onmouseleave="retirerAnimation(this)"/></a>
                         </div>
-                        <div class="rating">
-                            <div class="product-ratings d-inline-block align-middle">
-                                ${article.ratings}
+                        <div class="thumb-description">
+                            <div class="caption">
+                                <h4 class="product-title text-capitalize"><a href="${article.productLink}">${article.productTitle}</a></h4>
                             </div>
-                        </div>
-                        <div class="price">
-                            <div class="regular-price">${article.price}</div>
-                        </div>
-                        <div class="button-wrapper">   
-                            <div class="button-group text-center">
-                                <button type="button" class="btn btn-primary btn-cart" data-target="#cart-pop" data-toggle="modal"><i class="material-icons">shopping_cart</i><span>Add to cart</span></button>
-                                <a href="wishlist.html" class="btn btn-primary btn-wishlist"><i class="material-icons">favorite</i><span>wishlist</span></a>
-                            </div>          
+                            <div class="rating">
+                                <div class="product-ratings d-inline-block align-middle">
+                                    ${article.ratings}
+                                </div>
+                            </div>
+                            <div class="price">
+                                <div class="regular-price">${article.price}</div>
+                            </div>
+                            <div class="button-wrapper">   
+                                <div class="button-group text-center">
+                                    <button type="button" class="btn btn-primary btn-cart" data-target="#cart-pop" data-toggle="modal"><i class="material-icons">shopping_cart</i><span>Add to cart</span></button>
+                                    <a href="wishlist.html" class="btn btn-primary btn-wishlist"><i class="material-icons">favorite</i><span>wishlist</span></a>
+                                </div>          
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-            </div>
-            `;
-        }
-
+        `;
+        document.getElementById('articlesContainer').innerHTML += articleHTML;
     }
+}
