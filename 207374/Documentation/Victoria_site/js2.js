3cb30c0172701130756b63fad94a494b12d7116f
zinebfthdn <zineb.fetheddine.15@gmail.com>
2023-12-24 03:56:03 +0100

    Advices

---
+++ 207374/Documentation/Victoria_site/js2.js
@@ -0,0 +1,195 @@
+let classy = [
+    {
+        imageSrc: "img/products/p8.jpg",
+        secondImageSrc: "img/products/p9.jpg",
+        altText: "01",
+        secondAltText: "02",
+        productLink: "product-details.html",
+        productTitle: "Blazer + Bottom",
+        ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+        price: "$44.99"
+
+    
+    },
+    {
+        imageSrc: "img/products/p6.jpg",
+        secondImageSrc: "img/products/p7.jpg",
+        altText: "03",
+        secondAltText: "04",
+        productLink: "product-details.html",
+        productTitle: "Formal fit (Blazer + bottom)",
+        ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+        price: "$50.00"
+    },
+    {
+        imageSrc: "img/products/p10.jpg",
+        secondImageSrc: "img/products/p11.jpg",
+        altText: "04",
+        secondAltText: "05",
+        productLink: "product-details.html",
+        productTitle: "Formal fit (Blazer + Skirt)",
+        ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+        price: "$50.00"
+    },
+    {
+        imageSrc: "img/products/p12.jpg",
+        secondImageSrc: "img/products/p13.jpg",
+        altText: "06",
+        secondAltText: "07",
+        productLink: "product-details.html",
+        productTitle: "Woolen Coat",
+        ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+        price: "$50.00"
+    },
+    // Ajoutez d'autres articles pour le style 'classe' si nécessaire
+]
+let StreetWear= [
+    {
+        imageSrc: "img/products/s1.jpg",
+        secondImageSrc: "img/products/s2.jpg",
+        altText: "03",
+        secondAltText: "04",
+        productLink: "product-details.html",
+        productTitle: "Crochet Detail Top",
+        ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+        price: "$50.00"
+    },
+    {
+        imageSrc: "img/products/s3.jpg",
+        secondImageSrc: "img/products/s4.jpg",
+        altText: "03",
+        secondAltText: "04",
+        productLink: "product-details.html",
+        productTitle: "Crochet Detail Top",
+        ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+        price: "$50.00"
+    },
+    {
+        imageSrc: "img/products/s5.jpg",
+        secondImageSrc: "img/products/s6.jpg",
+        altText: "03",
+        secondAltText: "04",
+        productLink: "product-details.html",
+        productTitle: "Crochet Detail Top",
+        ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+        price: "$50.00"
+    },
+]
+    // Ajoutez d'autres articles pour le style 'streetwear' si nécessaire
+    let casual = [
+    {
+        imageSrc: "img/products/c1.jpg",
+        secondImageSrc: "img/products/c2.jpg",
+        altText: "03",
+        secondAltText: "04",
+        productLink: "product-details.html",
+        productTitle: "Knitted Lounge Set",
+        ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+        price: "$50.00"
+    },
+    {
+        imageSrc: "img/products/c3.jpg",
+        secondImageSrc: "img/products/c4.jpg",
+        altText: "03",
+        secondAltText: "04",
+        productLink: "product-details.html",
+        productTitle: "Crochet Detail Top",
+        ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+        price: "$50.00"
+    },
+    {
+        imageSrc: "img/products/c5.jpg",
+        secondImageSrc: "img/products/c6.jpg",
+        altText: "03",
+        secondAltText: "04",
+        productLink: "product-details.html",
+        productTitle: "Crochet Detail Top",
+        ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+        price: "$50.00"
+    },
+]
+
+let sporty = [
+    {
+        imageSrc: "img/products/sp1.jpg",
+        secondImageSrc: "img/products/sp2.jpg",
+        altText: "03",
+        secondAltText: "04",
+        productLink: "product-details.html",
+        productTitle: "Crochet Detail Top",
+        ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+        price: "$50.00"
+    },
+    {
+        imageSrc: "img/products/sp3.jpg",
+    secondImageSrc: "img/products/sp4.jpg",
+    altText: "03",
+    secondAltText: "04",
+    productLink: "product-details.html",
+    productTitle: "Crochet Detail Top",
+    ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+             '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+             '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+             '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+             '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+    price: "$50.00"
+},
+{
+    imageSrc: "img/products/s3.jpg",
+        secondImageSrc: "img/products/s4.jpg",
+        altText: "03",
+        secondAltText: "04",
+        productLink: "product-details.html",
+        productTitle: "Crochet Detail Top",
+        ratings: '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons">star</i></span>' +
+                 '<span class="fa fa-stack"><i class="material-icons off">star</i></span>',
+        price: "$50.00"
+    },
+]
